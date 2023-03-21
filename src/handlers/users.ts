d2414b743d18b3bba7e1a46b2ac541f4c4ba14dd
Taryn King <marshaeking@gmail.com>
2023-03-20 20:23:03 -0500

    feat: add auth logic

--- src/handlers/users.ts
+++ src/handlers/users.ts
@@ -1,5 +1,5 @@
 import prisma from '../db';
-import { createJWT, hashedPassword } from '../modules/auth';
+import { comparePasswords, createJWT, hashedPassword } from '../modules/auth';
 
 export const createNewUser = async (req: any, res: any) => {
     const user = await prisma.user.create({
@@ -11,4 +11,29 @@ export const createNewUser = async (req: any, res: any) => {
 
     const token = createJWT(user);
     res.json({ token });
+}
+
+
+export const signIn = async (req: any, res: any) => {
+    // check for username in database
+    const user = await prisma.user.findUnique({
+        where: {
+            username: req.body.username
+        }
+    });
+
+    // if username exists, compare passwords
+    if (user) {
+        const isValid = await comparePasswords(req.body.password, user.password);
+
+        if (!isValid) {
+            res.status(401).json({ message: 'Not authorized' })
+        }
+        // if password matches, create token and send onward
+        const token = createJWT(user);
+        res.status(200).json({ token });
+    }
+    // No user found, send 400
+    return res.json('unable to login')
+
 }
